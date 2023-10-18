05b54e79d63f0386c02c9690fee19da43933bdca
juh518 <ju_h518@naver.com>
2023-10-18 10:51:18 +0900

    feat: create 기능 학습

---
+++ src/app/create/page.js
@@ -0,0 +1,45 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+export default function Create() {
+    const router = useRouter();
+
+
+    return (
+        // 사용자와 상호작용하는 것은 서버컴포넌트에서 다루지 않음
+        // 'use client'를 사용해서 클라이언트 컴포넌트에서 사용해야 함
+        <form onSubmit={(e)=>{
+            e.preventDefault();
+            const title = e.target.title.value;
+            const body = e.target.body.value;
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({title, body})
+            }
+            fetch(`http://localhost:9999/posts`, options)
+            .then(res=>res.json())
+            .then(result=>{
+                console.log(result);
+                const lastid = result.id;
+                
+                // 라우터를 사용해서 방금 생성한 글로 리디렉션
+                router.refresh();
+                router.push(`/read/${lastid}`);
+            })
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title" />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body" />
+            </p>
+            <p>
+                <input type="submit" value="create" />
+            </p>
+        </form>
+    )
+}
